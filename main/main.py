cab529ed24e28027e3f8c89b03476a181c784627
Alison Lima <alison.juniolima@gmail.com>
2024-06-05 04:58:30 -0300

    Implementando atacar monstro

--- main/main.py
+++ main/main.py
@@ -1,4 +1,29 @@
+from models.items.item import Pocao,Arma, Espada, Arco, Cajado
+from models.personagem import Personagem
+from models.monstros.monstro import Monstro
 
 def main():
-    print('Início')
+    # Criando um personagem
+    personagem = Personagem("Aragorn", "Guerreiro")
+
+    # Criando um monstro
+    monstro = Monstro("Orc", 50)
+
+    # Criando alguns itens
+    pocao = Pocao("Poção de Cura", "medio", 30)
+    espada = Espada("Espada Longa", "grande", 25)
+    arco = Arco("Arco Longo", "grande", 20)
+    cajado = Cajado("Cajado Mágico", "medio", 15)
+
+    # Adicionando itens ao inventário do personagem
+    personagem.adicionar_item(pocao)
+    personagem.adicionar_item(espada)
+    personagem.adicionar_item(arco)
+    personagem.adicionar_item(cajado)
+
+    # Mostrando o inventário do personagem
+    personagem.mostrar_inventario()
+
+    # Atacando o monstro
+    personagem.atacar(monstro)
     
--- models/personagem.py
+++ models/personagem.py
@@ -1,3 +1,4 @@
+from items.item import Pocao
 class Personagem:
     ATRIBUTOS_PADRAO = {
         'Guerreiro': {'inteligencia': 5, 'forca': 15, 'agilidade': 10,'vida_maxima':150},
@@ -13,14 +14,27 @@ class Personagem:
         self.agilidade = Personagem.ATRIBUTOS_PADRAO[classe]['agilidade']
         self.vida_maxima = Personagem.ATRIBUTOS_PADRAO[classe]['vida_maxima']
         self.inventario = []
-
-    def curar(self,tamanho):
+        self.vida_atual = Personagem.ATRIBUTOS_PADRAO[classe]['vida_maxima']
+    # visualizar inventario 
+    def abrir_inventario(self):
       if self.inventário:
           print('Inventário:')
           for item in self.inventario:
             print(f'- {item.nome} - {item.tamanho}')
       else:
         print('O inventário está vazio!') 
+    
+    # curar o personagem 
+    def curar(self, tamanho):
+        for item in self.inventario:
+            if isinstance(item, Pocao) and item.tamanho == tamanho:
+                cura = item.usar()
+                self.vida += cura
+                self.inventario.remove(item)  # Remover a poção usada do inventário testar se está removendo
+                print(f"{self.nome} foi curado e agora tem {self.vida} pontos de vida.")
+                return
+        print(f"{self.nome} não possui uma poção de tamanho {tamanho} no inventário.")
+    
     
     def pegar_item(self, item):
       pass
